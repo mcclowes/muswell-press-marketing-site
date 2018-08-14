045e5061b6a9f9609a890955ff4b8a653223f82c
mcclowes <maxcc@me.com>
2018-08-14 15:47:56 +0100

    fix

--- src/index.js
+++ src/index.js
@@ -3,13 +3,3 @@ import App from "./App";
 import { render, } from "react-snapshot";
 
 render(<App />, document.getElementById("root"));
-
-//console.log("attempting to remove service workers...");
-navigator &&
-	navigator.serviceWorker &&
-	navigator.serviceWorker.getRegistrations().then(registrations => {
-		//console.log("service workers found:", registrations);
-		registrations.forEach(reg => {
-			reg.unregister();
-		});
-	});
